72a6481b0cee82a5cb2a0d02917ebcfd61c7639b
zhupetter <zhupetter@zhupetterdeMacBook-Pro.local>
2015-05-10 18:26:08 +0800

    Initial Commit

---
+++ Tasks/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Tasks
+//
+//  Created by zhupetter on 15/5/10.
+//  Copyright (c) 2015年 zhupetter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
